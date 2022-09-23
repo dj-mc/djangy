6b211a22d721d2e499679aa21e40d0d82e7ddea5
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-09-23 18:23:05 -0500

    Build Question/Choice models and install polls

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
+from datetime import datetime
 
-# Create your models here.
+
+class Question(models.Model):
+    def __str__(self) -> str:
+        return self.asked_question
+
+    asked_question = models.CharField(max_length=200)
+    date_published = models.DateTimeField("date published")
+
+    def was_recently_published(self) -> bool:
+        return self.date_published >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    def __str__(self) -> str:
+        return self.question_choice
+
+    # Each choice is related to a single question
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question_choice = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
