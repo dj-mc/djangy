79952444377ed248cb0941ad2db86e6289c7eb90
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-10-11 13:44:34 -0500

    Replace views with generics

--- polls/views.py
+++ polls/views.py
@@ -1,32 +1,65 @@
-from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from django.template import loader
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
+# from django.http import Http404, HttpResponse, HttpResponseRedirect
+# from django.template import loader
 
-def index(request):
-    last_five_questions = Question.objects.order_by("-date_published")[:5]
-    template = loader.get_template("polls/index.html")
-    context = {
-        "last_five_questions": last_five_questions,
-    }
-    return HttpResponse(template.render(context, request))
+# Index view
 
 
-def details(request, question_id):
-    try:
-        target_question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    # Using render() from django.shortcuts as shorthand
-    return render(request, "polls/details.html", {"question": target_question})
+# def index(request):
+#     last_five_questions = Question.objects.order_by("-date_published")[:5]
+#     template = loader.get_template("polls/index.html")
+#     context = {
+#         "last_five_questions": last_five_questions,
+#     }
+#     return HttpResponse(template.render(context, request))
 
 
-def results(request, question_id):
-    target_question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question": target_question})
+class IndexView(generic.ListView):
+    # Override default question_detail.html template
+    template_name = "polls/index.html"
+    # Override default {{ question_list }} context variable
+    context_object_name = "last_five_questions"
+
+    def get_queryset(self):
+        return Question.objects.order_by("-date_published")[:5]
+
+
+# Details view
+
+# def details(request, question_id):
+#     try:
+#         target_question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     # Using render() from django.shortcuts as shorthand
+#     return render(request, "polls/details.html", {"question": target_question})
+
+
+class DetailsView(generic.DetailView):  # Expects pk value from urls.py
+    # Generic views need to know their model
+    model = Question
+    template_name = "polls/details.html"
+
+
+# Results view
+
+# def results(request, question_id):
+#     target_question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/results.html", {"question": target_question})
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+
+# Voting view
 
 
 def vote(request, question_id):
