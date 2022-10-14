ee9acc6634669afa75722f123d9d46e8c3d35bf9
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-10-13 21:01:38 -0500

    Playground for setup_test_environment

---
+++ setup_test_env.py
@@ -0,0 +1,17 @@
+# How to run REPL scripts like this one:
+# echo 'import setup_test_env' | pdm run python3 manage.py shell
+
+from django.test import Client
+from django.test.utils import setup_test_environment
+from django.urls import reverse
+
+setup_test_environment()  # Install template renderer
+client = Client()
+
+response = client.get("/")
+print(response.status_code)
+
+response = client.get(reverse("polls:index"))
+print(response.status_code)
+print(response.content)
+print(response.context["last_five_questions"])
