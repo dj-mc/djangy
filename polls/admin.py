c635dea0f7d298227df50b78a9976966c544f037
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-11-30 21:48:49 -0600

    Customize how admin page displays data fields

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,35 @@
 # Register models
 
 from django.contrib import admin
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+
+# Or admin.StackedInline
+class InlineChoices(admin.TabularInline):
+    # Display 3 inline extra choice slots
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # Objects are displayed as str() by default
+    # instead use list_display to display specifics:
+    list_display = ("asked_question", "date_published", "was_recently_published")
+    list_filter = ["date_published"]  # Filter questions by date_published
+
+    # Search functionality uses a LIKE query on database
+    search_fields = ["asked_question"]  # Search for a question
+
+    # Customize how data fields are displayed
+    fieldsets = [
+        (None, {"fields": ["asked_question"]}),
+        ("Metadata", {"fields": ["date_published"]}),
+    ]
+    inlines = [InlineChoices]
+
+
+# Register Question as its own page
+# Also display 3 inline extra choice slots
+admin.site.register(Question, QuestionAdmin)
+# Also register Choice as its own (optional) page
+admin.site.register(Choice)
--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,7 @@
 import datetime
 
 from django.db import models
+from django.contrib import admin
 from django.utils import timezone
 
 
@@ -11,8 +12,14 @@ class Question(models.Model):
     asked_question = models.CharField(max_length=200)
     date_published = models.DateTimeField("date published")
 
+    @admin.display(
+        boolean=True,  # Green checkmark
+        ordering=date_published,  # Order by another field
+        # Replace variable name with description
+        description="Published recently?",
+    )
     def was_recently_published(self) -> bool:
-        now = timezone.now()
+        now = timezone.now()  # Is date_published between 24 hours and now?
         return now - datetime.timedelta(days=1) <= self.date_published <= now
 
 
@@ -20,7 +27,9 @@ class Choice(models.Model):
     def __str__(self) -> str:
         return self.question_choice
 
-    # Each choice is related to a single question
+    # ForeignKey fields display a <select> box, and also
+    # provide a "(+) Add another..." button on admin page
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    # Each choice is related to a single question
     question_choice = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
